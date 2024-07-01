f4c9505b44636598ceb6cfea5ebff979735ed07e
Microintel <135629446+microintel@users.noreply.github.com>
2024-07-01 08:32:30 +0530

    Add files via upload

---
+++ dipmarkcard/dcetDATAinLIST.js
@@ -0,0 +1,10 @@
+let dcetDATA={
+    //reg last  reg       name     D REG   RANK   D SCIRE DIP C
+    s005:["130CS21005","AMITH G S","J2495","1919","63.000","81"],
+    s062:["130CS21062","VIGNESH D"," J3331","1880","62.000","82.3"],
+    s012:["130CS21012","CHANDRASHEKHAR S","J1255","509","80","82"],
+    
+    //s000:[" "," "," "," "," "," "],
+    
+    
+    };
