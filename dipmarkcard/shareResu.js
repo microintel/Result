f52bcff4dbfadd77b7de94e19e18183d7c563fab
Microintel <135629446+microintel@users.noreply.github.com>
2025-02-03 19:27:22 +0530

    Add files via upload

---
+++ dipmarkcard/shareResu.js
@@ -0,0 +1,24 @@
+document.body.addEventListener("dblclick",()=>{
+sh();
+});
+let imgs = [
+    "https://raw.githubusercontent.com/microintel/endgram/main/photo/microintel%20stamp.png",
+    "https://raw.githubusercontent.com/microintel/endgram/main/photo/microintel%20marvel.jpg",
+    "https://raw.githubusercontent.com/microintel/endgram/main/photo/microintel%20OI.jpeg",
+    "https://raw.githubusercontent.com/microintel/endgram/main/photo/microintel_%20variant-34-min.png"];
+
+async function sh() {
+    let imgUrl = imgs[Math.floor(Math.random() * imgs.length)];
+    try {
+      let res = await fetch(imgUrl);
+        let blob = await res.blob();
+       let file = new File([blob], "image.jpg", { type: blob.type });
+        if (navigator.canShare && navigator.canShare({ files: [file] })) {
+            await navigator.share({
+        files: [file],
+                url: window.location.href
+            });} else {
+                            alert("Sharing not wirking");}
+    } catch (err) {alert("Problem:", err);
+    }
+}
