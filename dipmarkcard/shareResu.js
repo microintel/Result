79ff485489a7ecaf8a80919af52a9c2f819a8a0b
Microintel <135629446+microintel@users.noreply.github.com>
2025-02-03 19:47:40 +0530

    Add files via upload

--- dipmarkcard/shareResu.js
+++ dipmarkcard/shareResu.js
@@ -1,31 +1,37 @@
-document.body.addEventListener("dblclick",()=>{
-sh();
+document.body.addEventListener("dblclick", () => {
+    sh();
 });
+
 let imgs = [
     "https://raw.githubusercontent.com/microintel/endgram/main/photo/microintel%20stamp.png",
     "https://raw.githubusercontent.com/microintel/endgram/main/photo/microintel%20marvel.jpg",
     "https://raw.githubusercontent.com/microintel/endgram/main/photo/microintel%20OI.jpeg",
-    "https://raw.githubusercontent.com/microintel/endgram/main/photo/microintel_%20variant-34-min.png"];
+    "https://raw.githubusercontent.com/microintel/endgram/main/photo/microintel_%20variant-34-min.png"
+];
 
 async function sh() {
-let urlSh = new URL(window.location.href);
-urlSh.searchParams.set("regno",document.getElementById("i").value);
-urlSh.searchParams.set("sem",document.getElementById("se").value);
+    let urlSh = new URL(window.location.href);
+    let regno = document.getElementById("i").value;
+    let se = document.getElementById("se").value;
+    urlSh.searchParams.set("regno", regno);
+    urlSh.searchParams.set("sem", se);
+    
     let imgUrl = imgs[Math.floor(Math.random() * imgs.length)];
+    
     try {
-      let res = await fetch(imgUrl);
+        let res = await fetch(imgUrl);
         let blob = await res.blob();
-       let file = new File([blob], "image.jpg", { type: blob.type });
+        let file = new File([blob], "image.jpg", { type: blob.type });
+
         if (navigator.canShare && navigator.canShare({ files: [file] })) {
             await navigator.share({
-        files: [file],
-                url:urlSh.toString()
-            });} else {
-                            alert("Sharing not wirking");}
-    } catch (err) {alert("Problem:", err);
+                files: [file],
+                url: urlSh.toString()
+            });
+        } else {
+            alert("Sharing not wirking");
+        }
+            } catch (err) {
+        alert("Problem: " + err);
     }
 }
-
-
-
- 
