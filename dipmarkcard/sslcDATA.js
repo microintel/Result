09e7812d7014482b204edb01bbe6fdbf0f4a56c9
Microintel <135629446+microintel@users.noreply.github.com>
2025-07-15 16:30:12 +0530

    Add files via upload

--- dipmarkcard/sslcDATA.js
+++ dipmarkcard/sslcDATA.js
@@ -50,10 +50,77 @@ let sslcDATA={
     </tbody>
     </table>
   `,
-  s062:`ksksksks`
-
-    
-    
-
-    
+  s062:`
+  <h3 style="text-align:center;"> SSLC </h3>
+  <table>
+  <thead>
+  <tr>
+  <th class="left-align">Subject</th>
+  <th>Max M</th>
+  <th>Obt I</th>
+  <th>Obt E</th>
+  <th>Tot</th>
+  <th>Grade</th>
+  </tr>
+  </thead>
+  <tbody>
+  <tr>
+  <td class="left-align">KANNADA</td>
+  <td>125</td>
+  <td>24</td>
+  <td>090</td>
+  <td>114</td>
+  <td>A+</td>
+  </tr>
+  <tr>
+  <td class="left-align">ENGLISH</td>
+  <td>100</td>
+  <td>19</td>
+  <td>074</td>
+  <td>093</td>
+  <td>A+</td>
+  </tr>
+  <tr>
+  <td class="left-align">INFORMATION TECHNOLOGY</td>
+  <td>100</td>
+  <td>20</td>
+  <td>064</td>
+  <td>084</td>
+  <td>A</td>
+  </tr>
+  <tr>
+  <td class="left-align">MATHS</td>
+  <td>100</td>
+  <td>20</td>
+  <td>058</td>
+  <td>078</td>
+  <td>B</td>
+  </tr>
+  <tr>
+  <td class="left-align">SCIENCE</td>
+  <td>100</td>
+  <td>20</td>
+  <td>064</td>
+  <td>084</td>
+  <td>A</td>
+  </tr>
+  <tr>
+  <td class="left-align">SOCIAL SCIENCE</td>
+  <td>100</td>
+  <td>20</td>
+  <td>068</td>
+  <td>088</td>
+  <td>A</td>
+  </tr>
+  <tr>
+  <td class="left-align">Total : </td>
+  <td>625</td>
+  <td>123</td>
+  <td>418</td>
+  <td>541</td>
+  <td>A</td>
+  </tr>
+  </tbody>
+  </table>
+  `
     };
